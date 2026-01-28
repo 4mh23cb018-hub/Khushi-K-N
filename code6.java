979dc02f98c3d263eaad6e2584b2c7f9f1485e20
4mh23cb018-hub <4mh23cb018@gmail.com>
2026-01-28 15:38:23 +0530

    Add files via upload

---
+++ code6.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class code6 {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		}
+
+}
---
+++ codeone.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class codeone {
+	public static void main(String[] args) {
+		int a=10;
+		float b=15;
+		char c='A';
+		boolean x=true;
+    System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(x);
+	}
+}
